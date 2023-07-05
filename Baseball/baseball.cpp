87d7dc8d1b13679ac15e97fce42335547a2a505b
JinSeok Oh <jinseok0.oh@samsung.com>
2023-07-06 08:42:58 +0900

    First Commit for BaseBall

--- Baseball/baseball.cpp
+++ Baseball/baseball.cpp
@@ -1,4 +1,52 @@
+#include <string>
+#include <stdexcept>
+
+using namespace std;
+
+struct GuessResult
+{
+	bool solved;
+	int strikes;
+	int balls;
+};
+
 class Baseball
 {
-	
+public:
+	explicit Baseball(const std::string ans) : question(ans) {}
+	bool isDupliacatedNumber(std::string& guessNumber)
+	{
+		return guessNumber[0] == guessNumber[1]
+			|| guessNumber[0] == guessNumber[2]
+			|| guessNumber[1] == guessNumber[2];
+	}
+
+	void assertIllegalArgument(std::string& guessNumber)
+	{
+		if (guessNumber.length() != 3)
+			throw length_error("Must be three letters.");
+
+		for (auto ch : guessNumber)
+		{
+			if (ch >= '0' && ch <= '9')
+				continue;
+			throw invalid_argument("Must be number");
+		}
+
+		if (isDupliacatedNumber(guessNumber))
+		{
+			throw invalid_argument("Must not have the same number");
+		}
+	}
+
+	GuessResult guess(std::string guessNumber)
+	{
+		assertIllegalArgument(guessNumber);
+		if (guessNumber == question) {
+			return { true ,3, 0 };
+		}
+		return { false, 3, 0 };
+	}
+private:
+	string question;
 };
--- baseball_test/baseball_test.cpp
+++ baseball_test/baseball_test.cpp
@@ -1,6 +1,37 @@
 #include "pch.h"
 #include "../Baseball/baseball.cpp"
+#include <string>
 
-TEST(BaseballGame, TryGameTest) {
-  EXPECT_EQ(1, 1);
+using namespace std;
+
+class BaseballFixture : public testing::Test
+{
+public:
+	Baseball game {"123"};
+	void assertIllegalArgument(string guessNumber)
+	{
+		try
+		{
+			game.guess(guessNumber);
+			FAIL();
+		}
+		catch (exception e)
+		{
+			//PASS
+		}
+	}
+};
+
+TEST_F(BaseballFixture, ThrowExceptionWhenInvalidCase) {
+	assertIllegalArgument("12");
+	assertIllegalArgument("12s");
+	assertIllegalArgument("121");
+}
+
+TEST_F(BaseballFixture, ReturnSolvedResultIfMatchedNumber) {
+	GuessResult result = game.guess("123");
+
+	EXPECT_TRUE(result.solved);
+	EXPECT_EQ(3, result.strikes);
+	EXPECT_EQ(0, result.balls);
 }
